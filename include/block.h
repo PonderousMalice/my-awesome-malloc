108b2ce027d55400ab520dbb56f82a312fc4ee04
David CHARLET <david.charlet@epita.fr>
2016-10-29 01:43:37 +0200

    [ADD] merge & update divide

--- include/block.h
+++ include/block.h
@@ -1,18 +1,24 @@
 #ifndef BLOCK
 # define BLOCK
-# define MAX_SIZE_BLOCK 4096
-# define MIN_SIZE_BLOCK 8
 # define _DEFAULT_SOURCE
+# include "utils.h"
 # include <stddef.h>
+# include <stdint.h>
+# include <unistd.h>
 
-struct block 
+size_t g_MAX_SIZE_BLOCK;
+size_t g_MIN_SIZE_BLOCK;
+
+struct block
 {
   char free;
   size_t size;
   struct block *next;
 };
 
-void divide(struct block *b);
 void *ptr_data(struct block *b);
+void divide(struct block *b, size_t size);
 void init_block(struct block *b, char free, size_t size, struct block *next);
+void init_g();
+void merge(struct block *b);
 #endif
--- src/block.c
+++ src/block.c
@@ -1,18 +1,48 @@
 #include "block.h"
 
-void *ptr_data(struct block *b)
+void init_g()
 {
-  return (void*) b ^ sizeof(struct block);
+  g_MAX_SIZE_BLOCK = sysconf(_SC_PAGESIZE);
+  g_MIN_SIZE_BLOCK = round_ptwo(sizeof (struct block));
 }
 
-void divide(struct block *b)
+void *ptr_data(struct block *b)
 {
-  if (!b->free)
+  return b ? ++b : NULL;
+}
+
+void divide(struct block *b, size_t size)
+{
+  if (!b->free || b->size == size || b-> size == g_MIN_SIZE_BLOCK)
     return;
-  b->size /= 2;
-  struct block *buddy = b ^ b->size;
+  b->size >>= 1;
+  uintptr_t b_adr = b;
+  uintptr_t b_size = b->size;
+  uintptr_t adr_buddy = b_adr ^ b_size;
+  struct block *buddy = adr_buddy;
   init_block(buddy, 1, b->size, b->next);
   b->next = buddy;
+  divide(b, size);
+}
+
+void merge(struct block *b)
+{
+  if (b->size == g_MAX_SIZE_BLOCK)
+    return;
+  uintptr_t b_adr = b;
+  uintptr_t b_size = b->size;
+  uintptr_t adr_buddy = b_adr ^ b_size;
+  struct block *buddy = adr_buddy;
+  if (b->next == buddy)
+  {
+    b->size <<= 1;
+    b->next = buddy->next;
+  }
+  else
+  {
+    buddy->size <<= 1;
+    buddy->next = b->next;
+  }
 }
 
 void init_block(struct block *b, char free, size_t size, struct block *next)
