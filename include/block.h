631dbf0294953eda1d672888d8225387a4e9f7b2
David CHARLET <david.charlet@epita.fr>
2016-10-29 02:19:23 +0200

    [UPDATED] malloc, now using union to avoid explicit cast

--- include/block.h
+++ include/block.h
@@ -9,6 +9,12 @@
 size_t g_MAX_SIZE_BLOCK;
 size_t g_MIN_SIZE_BLOCK;
 
+union ublock
+{
+  uintptr_t uint;
+  struct block *ptr;
+};
+
 struct block
 {
   char free;
@@ -16,6 +22,7 @@ struct block
   struct block *next;
 };
 
+struct block *get_buddy(struct block *b);
 void *ptr_data(struct block *b);
 void divide(struct block *b, size_t size);
 void init_block(struct block *b, char free, size_t size, struct block *next);
--- src/block.c
+++ src/block.c
@@ -16,10 +16,7 @@ void divide(struct block *b, size_t size)
   if (!b->free || b->size == size || b-> size == g_MIN_SIZE_BLOCK)
     return;
   b->size >>= 1;
-  uintptr_t b_adr = b;
-  uintptr_t b_size = b->size;
-  uintptr_t adr_buddy = b_adr ^ b_size;
-  struct block *buddy = adr_buddy;
+  struct block *buddy = get_buddy(b);
   init_block(buddy, 1, b->size, b->next);
   b->next = buddy;
   divide(b, size);
@@ -29,10 +26,7 @@ void merge(struct block *b)
 {
   if (b->size == g_MAX_SIZE_BLOCK)
     return;
-  uintptr_t b_adr = b;
-  uintptr_t b_size = b->size;
-  uintptr_t adr_buddy = b_adr ^ b_size;
-  struct block *buddy = adr_buddy;
+  struct block *buddy = get_buddy(b);
   if (b->next == buddy)
   {
     b->size <<= 1;
@@ -52,4 +46,13 @@ void init_block(struct block *b, char free, size_t size, struct block *next)
   b->free = free;
   b->size = size;
   b->next = next;
+}
+
+struct block *get_buddy(struct block *b)
+{
+  union ublock block;
+  union ublock buddy;
+  block.ptr = b;
+  buddy.uint = block.uint ^ b->size;
+  return buddy.ptr;
 }
